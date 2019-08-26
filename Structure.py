b5db07d63f12aba289e238f67a0e23ac9871e7b4
MBottinIUT <39375197+MBottinIUT@users.noreply.github.com>
2019-08-26 23:57:49 +0200

    Add files via upload

---
+++ Structure.py
@@ -0,0 +1,21 @@
+# TITRE DE L'EXERCICE
+
+# Importation des modules nécessaires
+
+# Instanciation des objets nécessaires
+
+# Initialisation des variables
+
+# -------------------------
+# -- PROGRAMME PRINCIPAL --
+# -------------------------
+# Boucle infinie
+while True :
+    try :
+        # Code de l'exercice
+    except KeyboardInterrupt :
+		# Effacement de la matrice
+        sense.clear()
+        print ("Au revoir...")
+		# Quitte la boucle infinie
+        break
